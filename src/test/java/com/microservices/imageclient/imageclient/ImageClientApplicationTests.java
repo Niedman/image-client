92939c19f043d0502e0372a1d03cb41e94ad6885
Nino Matos <niedman@gmail.com>
2019-09-24 13:55:57 +0200

    Initial commit

---
+++ src/test/java/com/microservices/imageclient/imageclient/ImageClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.imageclient.imageclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
